bad80e3ad3df3d2fa5805a08a5ea4cf46b97c848
Freitas <jorge.freitas@accenture.com>
2021-03-10 18:11:34 -0300

    Ajuste porta

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = jsonServer.create();
 //const router = jsonServer.router(path.join(__dirname,'Db301220.json'));
 const router = jsonServer.router('Db301220.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
