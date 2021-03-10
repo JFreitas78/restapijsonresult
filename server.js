abb8ae93445e9b5aa7d306f8e8da6997f7799ba8
Freitas <jorge.freitas@accenture.com>
2021-03-10 17:35:30 -0300

    Configure

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname,'Db301220.json'));
+const middlewares = jsonServer.defaults();
+const port = process.env.port || 8080;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+    console.log(`Json server is running in ${port}`);
+});
